eb6af732c7ccaae69b1e75ea87f68af836a657d2
SAlina23 <139255157+SAlina23@users.noreply.github.com>
2023-07-11 21:39:41 +0500

    Add files via upload

---
+++ 13/Question13.ts
@@ -0,0 +1,6 @@
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+let transportationList = ["Hayabusa", "Toyota Supra mk4 ", "Kawasaki Ninja H2R", "Skyline GT-R34"]
+for (let i = 0; i < transportationList.length; i++){
+    console.log("I would like to own a " + transportationList [i] + ".");
+} 
---
+++ 3/Question3.js
@@ -0,0 +1,10 @@
+"use strict";
+//*Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let a = "Eric";
+console.log(a.toLowerCase());
+console.log(a.toUpperCase());
+function titlecase(a) {
+    let step1 = a.split('');
+    console.log(step1);
+}
+titlecase(a);
